e9a39b74d487ac46752765c38a71d477be307d6a
Makani Hagiwara <78979960+makanih808@users.noreply.github.com>
2023-05-07 16:46:52 -1000

    Delete Constants.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++
@@ -1,8 +0,0 @@
-package org.firstinspires.ftc.teamcode;
-
-public final class Constants {
-
-    public static final class NavXConstants {
-        public static final byte NAVX_DEVICE_UPDATE_RATE_HZ = 50;
-    }
-}
